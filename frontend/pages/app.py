ccde4e4d6715376aefed90378845a27696d5c74f
S Shariq <shariqk5002@gmail.com>
2025-03-30 05:24:20 +0530

    adding feedback

--- frontend/pages/app.py
+++ frontend/pages/app.py
@@ -4,7 +4,6 @@ import json
 import os
 # API URL (replace with your actual endpoint)
 API_URL = os.getenv("API_URL", "https://heart-attack-predictor-c7gt.onrender.com/predict")
-
 # Streamlit UI
 st.title("AI Healthcare Disease Prediction")
 st.write("Enter your symptoms to check for possible disease.")
@@ -42,3 +41,14 @@ if st.button("Predict"):
 
     except Exception as e:
         st.error(f"Error connecting to API: {e}")
+
+# Google Form Feedback Link
+feedback_url = "https://docs.google.com/forms/d/e/1FAIpQLSeuQTgRd8heAxNuYYitS3QzJTWyGHtp1TNsLG9Fxl1GUWJpTQ/viewform?usp=dialog"
+
+st.title("💬 User Feedback")
+st.write("We'd love to hear your thoughts! Your feedback helps us improve.")
+
+# Button to redirect to Google Form
+if st.button("Give Feedback"):
+    st.markdown(f"[Click here to submit feedback]({feedback_url})", unsafe_allow_html=True)
+
